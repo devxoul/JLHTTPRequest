2ab22bf82c4027866623029b594f886efb2ea466
전수열 <devxoul@gmail.com>
2012-11-19 21:26:25 +0900

    Initial Commit

---
+++ JLHTTPRequest/JLHTTPRequest.h
@@ -0,0 +1,13 @@
+//
+//  JLHTTPRequest.h
+//  JLHTTPRequest
+//
+//  Created by 전수열 on 12. 11. 19..
+//  Copyright (c) 2012년 Joyfl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JLHTTPRequest : NSObject
+
+@end
