1183cbe604b6f4b376888e264b71e00a8024fd7c
전수열 <devxoul@gmail.com>
2012-11-22 20:10:48 +0900

    Removed loadStringAsyncFromURL

--- JLHTTPRequest/JLHTTPRequest/JLHTTPLoader.m
+++ JLHTTPRequest/JLHTTPRequest/JLHTTPLoader.m
@@ -132,18 +132,4 @@
 	} );
 }
 
-+ (NSString *)loadStringAsyncFromURL:(NSString *)url completion:(void (^)(NSData *data))completion
-{
-	dispatch_async( dispatch_get_global_queue( 0, 0 ), ^{
-		NSData *data = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:url]];
-		
-		if( data == nil )
-			return;
-		
-		dispatch_async( dispatch_get_main_queue(), ^{
-			completion( data );
-		} );
-	} );
-}
-
 @end
