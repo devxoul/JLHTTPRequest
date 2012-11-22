399ce9bcfd8d46a42cbb32478c8bdc9227e46f06
전수열 <devxoul@gmail.com>
2012-11-22 11:55:15 +0900

    Removed if( data == nil ) return;

--- JLHTTPRequest/JLHTTPRequest/JLHTTPLoader.m
+++ JLHTTPRequest/JLHTTPRequest/JLHTTPLoader.m
@@ -126,9 +126,6 @@
 	dispatch_async( dispatch_get_global_queue( 0, 0 ), ^{
 		NSData *data = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:url]];
 		
-		if( data == nil )
-			return;
-		
 		dispatch_async( dispatch_get_main_queue(), ^{
 			completion( data );
 		} );
